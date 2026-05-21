4aab566f3138b2e652be95f3727cbe5ebfb7b75a
Maciej Urban <maciej-urban@outlook.com>
2026-05-21 17:00:11 +0200

    move new/delete to context.hh

--- src/context.hh
+++ src/context.hh
@@ -4,36 +4,6 @@
 
 #include "allocator.hh"
 
-Allocator logging_default_allocator = {
-    .alloc = [](void*, std::size_t size, std::size_t alignment) -> void* {
-        std::print("default alloc: {} align: {}\n", size, alignment);
-        if (alignment <= DEFAULT_ALIGNMENT)
-            return std::malloc(size);
-        return std::aligned_alloc(alignment, size);
-    },
-    .free =
-        [](void*, void* pointer, std::size_t) {
-            std::print("default free: {}\n", pointer);
-            std::free(pointer);
-        },
-    .data = nullptr,
-};
-
-Allocator temp_allocator = {
-    .alloc = [](void*, std::size_t size, std::size_t alignment) -> void* {
-        std::print("temp alloc: {} align: {}\n", size, alignment);
-        if (alignment <= DEFAULT_ALIGNMENT)
-            return std::malloc(size);
-        return std::aligned_alloc(alignment, size);
-    },
-    .free =
-        [](void*, void* pointer, std::size_t) {
-            std::print("temp free: {}\n", pointer);
-            std::free(pointer);
-        },
-    .data = nullptr,
-};
-
 struct Context {
     Allocator allocator = default_allocator;
 };
@@ -43,11 +13,14 @@ thread_local Context context;
 struct Allocator_Scope {
     Allocator previous;
 
-    explicit Allocator_Scope(Allocator a) : previous(context.allocator) {
+    explicit Allocator_Scope(Allocator a)
+        : previous(context.allocator) {
         context.allocator = a;
     }
 
-    ~Allocator_Scope() { context.allocator = previous; }
+    ~Allocator_Scope() {
+        context.allocator = previous;
+    }
 
     Allocator_Scope(const Allocator_Scope&) = delete;
     Allocator_Scope& operator=(const Allocator_Scope&) = delete;
@@ -55,4 +28,112 @@ struct Allocator_Scope {
 
 #define PushAllocator(a) Allocator_Scope _alloc_scope_##__LINE__(a)
 
-#include "operator_new_delete.hh"
+//
+// Overwriting of operator new/delete to use context.allocator
+//
+
+void* operator new(std::size_t size) {
+    if (size == 0)
+        size = 1;
+    void* pointer = context.allocator.alloc(context.allocator.data, size, DEFAULT_ALIGNMENT);
+    if (!pointer)
+        throw std::bad_alloc{};
+    return pointer;
+}
+
+void* operator new[](std::size_t size) {
+    return ::operator new(size);
+}
+
+void* operator new(std::size_t size, const std::nothrow_t&) noexcept {
+    try {
+        return ::operator new(size);
+    } catch (...) {
+        return nullptr;
+    }
+}
+
+void* operator new[](std::size_t size, const std::nothrow_t&) noexcept {
+    try {
+        return ::operator new[](size);
+    } catch (...) {
+        return nullptr;
+    }
+}
+
+void* operator new(std::size_t size, std::align_val_t al) {
+    if (size == 0)
+        size = 1;
+    void* pointer = context.allocator.alloc(context.allocator.data, size, static_cast<std::size_t>(al));
+    if (!pointer)
+        throw std::bad_alloc{};
+    return pointer;
+}
+
+void* operator new[](std::size_t size, std::align_val_t al) {
+    return ::operator new(size, al);
+}
+
+void* operator new(std::size_t size, std::align_val_t al, const std::nothrow_t&) noexcept {
+    try {
+        return ::operator new(size, al);
+    } catch (...) {
+        return nullptr;
+    }
+}
+
+void* operator new[](std::size_t size, std::align_val_t al, const std::nothrow_t&) noexcept {
+    try {
+        return ::operator new[](size, al);
+    } catch (...) {
+        return nullptr;
+    }
+}
+
+void operator delete(void* pointer) noexcept {
+    context.allocator.free(context.allocator.data, pointer, DEFAULT_ALIGNMENT);
+}
+
+void operator delete[](void* pointer) noexcept {
+    ::operator delete(pointer);
+}
+
+void operator delete(void* pointer, const std::nothrow_t&) noexcept {
+    ::operator delete(pointer);
+}
+
+void operator delete[](void* pointer, const std::nothrow_t&) noexcept {
+    ::operator delete[](pointer);
+}
+
+void operator delete(void* pointer, std::size_t) noexcept {
+    ::operator delete(pointer);
+}
+
+void operator delete[](void* pointer, std::size_t) noexcept {
+    ::operator delete[](pointer);
+}
+
+void operator delete(void* pointer, std::align_val_t al) noexcept {
+    context.allocator.free(context.allocator.data, pointer, static_cast<std::size_t>(al));
+}
+
+void operator delete[](void* pointer, std::align_val_t al) noexcept {
+    ::operator delete(pointer, al);
+}
+
+void operator delete(void* pointer, std::size_t, std::align_val_t al) noexcept {
+    ::operator delete(pointer, al);
+}
+
+void operator delete[](void* pointer, std::size_t, std::align_val_t al) noexcept {
+    ::operator delete(pointer, al);
+}
+
+void operator delete(void* pointer, const std::nothrow_t&, std::align_val_t al) noexcept {
+    ::operator delete(pointer, al);
+}
+
+void operator delete[](void* pointer, const std::nothrow_t&, std::align_val_t al) noexcept {
+    ::operator delete(pointer, al);
+}
