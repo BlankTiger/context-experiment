d78d3645191548a7a7667d7c2c879d43a1e04f5d
Maciej Urban <maciej-urban@outlook.com>
2026-05-21 17:00:19 +0200

    experiments

--- src/main.cc
+++ src/main.cc
@@ -1,52 +1,60 @@
 #include "arena.hh"
 #include "context.hh"
+#include <iomanip>
+#include <iostream>
 #include <print>
-#include <thread>
+#include <string>
+#include <utility>
 #include <vector>
 
-thread_local int thread_id;
-const auto MAX_ITERATIONS = 100;
-
-void print_thread_info() {
-    std::print("Thread {}, allocator pointer: {}\n", thread_id,
-               static_cast<void*>(&context.allocator));
-}
-
-Arena arena;
+Arena<true> arena;
 const auto arena_allocator = arena.allocator();
 
-void using_temp() {
-    thread_id = 3;
+Arena<true> arena2;
+const auto arena_allocator2 = arena2.allocator();
+
+std::pair<std::vector<int>, std::string> using_temp() {
     PushAllocator(arena_allocator);
 
-    for (size_t i{0}; i < MAX_ITERATIONS; ++i) {
-        print_thread_info();
-        std::print("memory left: {}\n", arena.space_left());
-        std::vector v{1, 2, 3};
-
-        if (i % 5 == 0)
-            arena.reset();
-    }
+    std::print("memory left: {}\n", arena.bytes_left());
+    std::vector v{1, 2, 3};
+    std::string a{"ssssssssssssssssssssssssssssssssssssssssssssssss"};
+    std::print("memory left: {}\n", arena.bytes_left());
+    return {std::move(v), std::move(a)};
 }
 
-void using_default() {
-    thread_id = 2;
-    PushAllocator(logging_default_allocator);
-
-    for (size_t i{0}; i < MAX_ITERATIONS; ++i) {
-        print_thread_info();
-        std::vector v{1, 2, 3};
+void print_span(std::span<const std::byte> bytes) {
+    for (std::byte b : bytes) {
+        std::cout << std::hex << std::setw(2) << std::setfill('0')
+                  << static_cast<unsigned>(std::to_integer<unsigned>(b)) << ' ';
     }
+    std::cout << std::dec << '\n';
 }
 
 int main() {
-    thread_id = 1;
-    print_thread_info();
+    auto [v, s] = using_temp();
+    std::print("{}, {}\n", v, s);
 
-    std::jthread t1(using_temp);
-    t1.join();
+    {
+        PushAllocator(arena_allocator2);
+        auto v2 = v;
+        auto s2 = s;
+        std::print("{}, {} 2\n", v2, s2);
+    }
 
-    print_thread_info();
+    {
+        auto v2 = v;
+        auto s2 = s;
+        std::print("{}, {} 3\n", v2, s2);
+    }
+
+    // std::span arena_memory{arena.memory_base, 150};
+    // print_span(arena_memory);
+    //
+    // arena.reset();
+    //
+    // std::print("{}, {}\n", *v, *s);
+    // print_span(arena_memory);
 
     return 0;
 }
